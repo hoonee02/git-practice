db979c84a21a5bc11112dc16f76cbd22239e619c
hoonee02 <hoonee02@naver.com>
2023-05-01 11:21:58 +0900

    modifying main.py

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 # Test Python env
 
-print('hello')
+def print_hello():
+    animals = ['dog','cat','hamster'] # in one line
+    foods = [
+            'Spagetti',
+            'Pizza'
+            ] # w/o trailing comma
+    names = [
+            'john',
+            'jane',
+            'gil-dong',
+            ] # w/ trailing comma
+    for f_name in names:
+        print(f'hello, {f_name}')
+if __name__ == '__main__':
+    print_hello()
